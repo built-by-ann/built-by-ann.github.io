6af8b986c1b7852b55a95a5072218aedb9d5e830
built-by-ann <ann-e-mathew154@gmail.com>
2025-05-07 16:15:06 -0400

    src

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/built-by-ann.github.io/', // ✅ required for GitHub Pages
+  base: '/built-by-ann.github.io/',
   plugins: [react()],
 });
