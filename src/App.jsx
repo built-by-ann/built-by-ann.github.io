b030eb810a9be648c372a91fefda57044c040eaf
built-by-ann <ann-e-mathew154@gmail.com>
2025-05-07 08:27:52 -0400

    App.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/built-by-ann.github.io">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
